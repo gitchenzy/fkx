15e41c64340a53af8c54c672cbd1a67d93dd84f0
clear <625244441@qq.com>
2017-12-13 22:09:05 +0800

    li

--- Application/Home/Controller/ListsController.class.php
+++ Application/Home/Controller/ListsController.class.php
@@ -16,16 +16,63 @@ class ListsController extends Controller {
 		session('key',2);
 		//查询楼盘
 		//查询总价、
+		$where['is_del'] = 0;
+		$where['status'] = 1;
+		$data = I('get.');
+		if(isset($data['hx']) && !empty($data['hx'])){
+			$where['huxing_id'] = $data['hx'];
+		}
+		if(isset($data['zx']) && !empty($data['zx'])){
+			$where['zxiu_id'] = $data['zx'];
+		}
+		if(isset($data['zj']) && !empty($data['zj'])){
+			$where['jiage_id'] = $data['zj'];
+		}
+		if(isset($data['ty']) && !empty($data['ty'])){
+			$where['type_id'] = $data['ty'];
+		}
+		if(isset($data['cy']) && !empty($data['cy'])){
+			$where['area_id'] = $data['cy'];
+		}
+		if(isset($data['sh']) && !empty($data['sh'])){
+			$where['title|address'] = array('like',"%{$data['sh']}%");
+		}
+		$info = M('loupan') -> where($where) -> order('id desc') ->limit(0,5) -> select();
 		$jiage = M('jiage') -> where(['is_del'=>0]) -> select();
-		$huxing = M('huxing') -> where(['is_del'=>0]) -> select();
+		$huxings = M('huxing') -> where(['is_del'=>0]) -> select();
 		$zxiu = M('zxiu') -> where(['is_del'=>0]) -> select();
 		$types = M('types') -> where(['is_del'=>0]) -> select();
-		$info = M('loupan') -> where(['is_del' => 0,'status'=>1]) -> order('id desc') ->limit(0,5) -> select();
+		$city = M('citys') -> where(['is_del'=>0,'pid'=>0]) -> select();
+		foreach($city as &$v){
+			$v['son'] =  M('citys') -> where(['is_del'=>0,'pid'=>$v['id']]) -> select();
+		}
+	//	$u_info = $_SERVER['QUERY_STRING'];
+
+	//	unset($hx_arr['hx']);
+
+		parse_str($_SERVER['QUERY_STRING'],$arr);
+		$sh = $cy = $zj = $ty = $zx = $huxing = $arr;
+		unset($huxing['hx']);
+		$url['hx'] = '/index.php/Home/Lists/index?'.http_build_query($huxing);
+		unset($zx['zx']);
+		$url['zx'] = '/index.php/Home/Lists/index?'.http_build_query($zx);
+		unset($ty['ty']);
+		$url['ty'] = '/index.php/Home/Lists/index?'.http_build_query($ty);
+		unset($zj['zj']);
+		$url['zj'] = '/index.php/Home/Lists/index?'.http_build_query($zj);
+		unset($cy['cy']);
+		$url['cy'] = '/index.php/Home/Lists/index?'.http_build_query($cy);
+		unset($sh['sh']);
+		$url['sh'] = '/index.php/Home/Lists/index?'.http_build_query($sh);
+		$pagewhere = http_build_query($arr);
 		$this -> assign('jiage',$jiage);
-		$this -> assign('huxing',$huxing);
+		$this -> assign('huxing',$huxings);
 		$this -> assign('zxiu',$zxiu);
 		$this -> assign('types',$types);
 		$this -> assign('info',$info);
+		$this -> assign('city',$city);
+		$this -> assign('url',$url);
+		$this -> assign('pagewhere',$pagewhere);
 		$this -> display();
 	}
 
@@ -33,6 +80,26 @@ class ListsController extends Controller {
 		$where['is_del'] = 0;
 		$where['status'] = 1;
 		$count = I('time');
+		$pagewhere = I('where');
+		parse_str($pagewhere,$data);
+		if(isset($data['hx']) && !empty($data['hx'])){
+			$where['huxing_id'] = $data['hx'];
+		}
+		if(isset($data['zx']) && !empty($data['zx'])){
+			$where['zxiu_id'] = $data['zx'];
+		}
+		if(isset($data['zj']) && !empty($data['zj'])){
+			$where['jiage_id'] = $data['zj'];
+		}
+		if(isset($data['ty']) && !empty($data['ty'])){
+			$where['type_id'] = $data['ty'];
+		}
+		if(isset($data['cy']) && !empty($data['cy'])){
+			$where['area_id'] = $data['cy'];
+		}
+		if(isset($data['sh']) && !empty($data['sh'])){
+			$where['title|address'] = array('like',"%{$data['sh']}%");
+		}
 		$info = M('loupan') -> where($where) -> order('id desc') ->limit(5*$count,5) -> select();
 		//   dump($project);
 		if($info){
